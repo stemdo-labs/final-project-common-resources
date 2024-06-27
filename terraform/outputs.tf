60d55808a90ba2d31bc73c2992bfc44f62b2829f
sebasnaa <wsntsebastian@gmail.com>
2024-06-27 16:15:27 +0200

    deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,1 @@
-output "aks_id" {
-  value       = azurerm_kubernetes_cluster.aks.id
-  description = "The Kubernetes Managed Cluster ID."
-}
-
-
-output "properties" {
-  value       = azurerm_kubernetes_cluster.aks
-  description = "An object with all of the AKS' terraform properties."
-}
+# hi
