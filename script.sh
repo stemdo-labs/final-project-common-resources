3927678b1ae13e43e8c5395578c494a8a04bcb62
sabreu-stemdo <146743783+sabreu-stemdo@users.noreply.github.com>
2024-06-28 09:13:22 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-            for dir in k8s/*; do
-              if [ -d "$dir" ]; then
-                for file in "$dir"/*.yaml; do
-                  echo "$file"
-                done
-              fi
-            done
